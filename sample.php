1f243275f1d2c63611df1f7caa3cca37d5b23470
Jeric James Viernes <jericviernes06@gmail.com>
2024-04-18 02:18:44 +0800

    tweeks

--- sample.php
+++ sample.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,17 +9,4 @@
 <body>
     <p>HELLOs</p>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="src/output.css" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
-    <p>HELLOs</p>
-</body>
->>>>>>> f0c8d8f (initial commit)
 </html>
